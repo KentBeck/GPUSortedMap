09b941974abb76316e8eaf261ec526606c640ca7
Kent Beck <kentlbeck@gmail.com>
2026-01-27 09:19:13 -0800

    Move GpuArray into module

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
+mod gpu_array;
+
 use bytemuck::{Pod, Zeroable};
-use std::marker::PhantomData;
 use std::sync::Arc;
+
+use crate::gpu_array::{GpuArray, GpuStorage};
 
 #[repr(C)]
 #[derive(Clone, Copy, Pod, Zeroable, Debug, Default)]
@@ -17,122 +20,6 @@ pub struct ResultEntry {
     pub _pad: [u32; 2],
 }
 
-#[repr(C)]
-#[derive(Clone, Copy, Pod, Zeroable, Debug, Default)]
-pub struct SlabMeta {
-    pub len: u32,
-    pub capacity: u32,
-    pub _pad: [u32; 2],
-}
-
-pub struct GpuArray<T: Pod> {
-    buffer: wgpu::Buffer,
-    meta_buffer: wgpu::Buffer,
-    capacity: u32,
-    len: u32,
-    _marker: PhantomData<T>,
-}
-
-pub struct GpuStorage<T: Pod> {
-    buffer: wgpu::Buffer,
-    _marker: PhantomData<T>,
-}
-
-impl<T: Pod> GpuStorage<T> {
-    pub fn new(device: &wgpu::Device, usage: wgpu::BufferUsages, label: &str) -> Self {
-        let size = std::mem::size_of::<T>() as u64;
-        let buffer = device.create_buffer(&wgpu::BufferDescriptor {
-            label: Some(label),
-            size,
-            usage,
-            mapped_at_creation: false,
-        });
-        Self {
-            buffer,
-            _marker: PhantomData,
-        }
-    }
-
-    pub fn buffer(&self) -> &wgpu::Buffer {
-        &self.buffer
-    }
-}
-
-impl<T: Pod> GpuArray<T> {
-    pub fn new(
-        device: &wgpu::Device,
-        capacity: u32,
-        buffer_usage: wgpu::BufferUsages,
-        label: &str,
-    ) -> Self {
-        let size = (capacity as u64) * std::mem::size_of::<T>() as u64;
-        let buffer = device.create_buffer(&wgpu::BufferDescriptor {
-            label: Some(label),
-            size,
-            usage: buffer_usage,
-            mapped_at_creation: false,
-        });
-
-        let meta = SlabMeta {
-            len: 0,
-            capacity,
-            _pad: [0; 2],
-        };
-        let meta_size = std::mem::size_of::<SlabMeta>() as u64;
-        let meta_buffer = device.create_buffer(&wgpu::BufferDescriptor {
-            label: Some("slab-meta-buffer"),
-            size: meta_size,
-            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
-            mapped_at_creation: true,
-        });
-        {
-            let mut view = meta_buffer.slice(..).get_mapped_range_mut();
-            view.copy_from_slice(bytemuck::bytes_of(&meta));
-        }
-        meta_buffer.unmap();
-
-        Self {
-            buffer,
-            meta_buffer,
-            capacity,
-            len: 0,
-            _marker: PhantomData,
-        }
-    }
-
-    pub fn buffer(&self) -> &wgpu::Buffer {
-        &self.buffer
-    }
-
-    pub fn meta_buffer(&self) -> &wgpu::Buffer {
-        &self.meta_buffer
-    }
-
-    pub fn capacity(&self) -> u32 {
-        self.capacity
-    }
-
-    pub fn len(&self) -> u32 {
-        self.len
-    }
-
-    pub fn update_len(&mut self, queue: &wgpu::Queue, new_len: u32) {
-        self.len = new_len.min(self.capacity);
-        let meta = SlabMeta {
-            len: self.len,
-            capacity: self.capacity,
-            _pad: [0; 2],
-        };
-        queue.write_buffer(&self.meta_buffer, 0, bytemuck::bytes_of(&meta));
-    }
-
-    pub fn write(&self, queue: &wgpu::Queue, data: &[T]) {
-        if data.is_empty() {
-            return;
-        }
-        queue.write_buffer(&self.buffer, 0, bytemuck::cast_slice(data));
-    }
-}
 
 #[repr(C)]
 #[derive(Clone, Copy, Pod, Zeroable, Debug, Default)]
